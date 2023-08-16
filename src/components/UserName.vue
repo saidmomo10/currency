86aec4cec79e81f3225cf70c656a18618d7d8878
saidmomo10 <saidoumohamef2@gmail.com>
2023-08-16 03:57:27 +0100

    dernier_commit

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -4,7 +4,6 @@
 
 <script setup lang="ts">
     import { ref } from 'vue';
-    import SignupForm from '../components/SignupForm.vue'
     import {supabase} from '@/clients/supabase'
 
     const userEmail = ref();
@@ -12,7 +11,7 @@
 async function getSession(){
     const user = supabase.auth.getUser();
       if (user) {
-        userEmail.value = (await user).data.user?.email.split("@")[0];
+        userEmail.value = (await user).data.user?.email;
       }
 }
 
